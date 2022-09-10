abfaf23c323abfe1f68125dfad2698ccb1786656
Mridul1703 <mridulvasudeva.03@gmail.com>
2022-09-10 18:43:12 +0530

    Resolved the errors, optimized the code, added classes(0%,12%,18%,28%) and some examples

--- Main.java
+++ Main.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
 public class Main {
-	int n;//counter var for loop
-	int ch;//choice
+    int n;//counter var for loop
 	public static void main(String[] args) {
 	Scanner sc = new Scanner (System.in);
 	int n=1;
+	int qt;
+	float mr;
 
 	while(n==1) {
 		System.out.println("\nSelect a category");
 		System.out.println("\t1 for EATABLES");
 		System.out.println("\t2 for AUTOMOTIVE");
-		System.out.printlm("\t3 for DAILY ESSENTIALS");
-		System.out.println("\t5 for ELECTRONICS");
-		System.out.println("\t6 for FASHION");
-		System.out.println("\t7 for TRAVEL");
-		System.out.println("\t8 to Exit");
+		System.out.println("\t3 for DAILY ESSENTIALS");
+		System.out.println("\t4 for ELECTRONICS");
+		System.out.println("\t5 for FASHION");
+		System.out.println("\t6 for TRAVEL");
+		System.out.println("\t7 to Exit");
 		System.out.print("Enter your choice: ");
 		int ch=sc.nextInt();
 
@@ -52,10 +53,11 @@ public class Main {
 
 				switch (ch1) {
 					case 1:
+						//Rice , gst-5%//
 						System.out.print("\nEnter quantity: "); 
-						int qt = sc.nextInt();
+						qt = sc.nextInt();
 						System.out.print("Enter MRP: ");
-						float mr = sc.nextFloat();
+						mr = sc.nextFloat();
 						FivePercent f = new FivePercent(qt, mr);
 						f.Invoice();
 						break;
@@ -67,12 +69,24 @@ public class Main {
 						break;
 					case 4:
 						//Wheat , gst-0%//
+						System.out.print("\nEnter quantity: "); 
+						qt = sc.nextInt();
+						System.out.print("Enter MRP: ");
+						mr = sc.nextFloat();
+						ZeroPercent wh = new ZeroPercent(qt, mr);
+						wh.Invoice();
 						break;
 					case 5:
 						//Oil , gst-5%//
 						break;
 					case 6:
 						//Ghee , gst-12%//
+						System.out.print("\nEnter quantity: "); 
+						qt = sc.nextInt();
+						System.out.print("Enter MRP: ");
+						mr = sc.nextFloat();
+						TwelvePercent gh = new TwelvePercent(qt, mr);
+						gh.Invoice();
 						break;
 					case 7:
 						//Poha , gst-5%//
@@ -85,6 +99,12 @@ public class Main {
 						break;
 					case 10:
 						//Biscuits , gst-18%//
+						System.out.print("\nEnter quantity: "); 
+						qt = sc.nextInt();
+						System.out.print("Enter MRP: ");
+						mr = sc.nextFloat();
+						EighteenPercent bst = new EighteenPercent(qt, mr);
+						bst.Invoice();
 						break;
 					case 11:
 						//Namkeen , gst-12%//
@@ -130,12 +150,15 @@ public class Main {
 						break;
 					case 25:
 						//Caffinated Beverages , gst-28%//
+						System.out.print("\nEnter quantity: "); 
+						qt = sc.nextInt();
+						System.out.print("Enter MRP: ");
+						mr = sc.nextFloat();
+						TwentyEightPercent bvg = new TwentyEightPercent(qt, mr);
+						bvg.Invoice();
 						break;
-						
-					default:
-						System.out.println("Invalid Choice");
 				}
-		switch (ch) {
+				
 			case 2:
 				System.out.println("\nSelect an item");
 				System.out.println("\t1 for Helmets");
@@ -189,7 +212,7 @@ public class Main {
 				default:
 					System.out.println("Invalid Choice");
 				}
-		switch (ch) {
+				
 			case 3:
 				System.out.println("\nSelect an item");
 				System.out.println("\t1 for brush");
@@ -215,7 +238,6 @@ public class Main {
 				int ch3=sc.nextInt();
 				
 		        switch (ch3) {
-		        
 				    case 1:
 				    	//brush , gst -18%//
 						break;
@@ -276,68 +298,18 @@ public class Main {
 					default:
 						System.out.println("Invalid Choice");
 					}
-		    switch (ch) {
-		       case 4:
-		    	    System.out.println("\nSelect an item");
-					System.out.println("\t1 for Washing Machine");
-					System.out.println("\t2 for Air Conditioner");
-					System.out.println("\t3 for Dish Washing Machine");
-					System.out.println("\t4 for Mobile Phone");
-					System.out.println("\t5 for Air Podes");
-					System.out.println("\t6 for Speakers");
-					System.out.println("\t7 for Laptop");
-					System.out.println("\t8 for Hair Appliances");
-					System.out.println("\t9 for Mixer Grinder");
-					System.out.println("\t10 for Water Purifier");
-					System.out.println("\t11 for Water Gyser");
-					System.out.println("\t12 for Printer");
-					System.out.println("\t13 for TV(Above 32 inches)");
-					System.out.println("\t14 for Smart Watches");
-					System.out.println("\t15 for Sewing Machine");
-					int ch4=sc.nextInt();
-					break;
-		    	   
-		    	
-		   switch (ch) {
-		      case 5:
-		    	    System.out.println("\nSelect an item");
-					System.out.println("\t1 for Leather Products");
-					System.out.println("\t2 for Footwear");
-					System.out.println("\t3 for Watches");
-					System.out.println("\t4 for Clothing");
-					System.out.println("\t5 for Perfumes");
-					System.out.println("\t6 for Sunscream");
-					System.out.println("\t7 for Cosmetic Products");
-					System.out.println("\t8 for Trimmer");
-					System.out.println("\t9 for Kids Cotton Clothes");
-					int ch5=sc.nextInt();
-					break;
-		    	  
-		        
-		   }
-			break;
 			
-		 default:
-			 System.out.println("Invalid Choice");
-				System.out.println("Enter 1 to add an item and 2 to ");
-				}
-			}
-		}
-	}
+		    case 7: 
+		        n = 0;
+		        break;
+	    }
+    }
+}
+}
 					
 class GST {
 	float Gst, Cgst, Sgst, rate, FinalAmount, mrqt;
 	// Final Amount = rate + Cgst + Sgst
-}
-
-class FivePercent extends GST {
-	FivePercent(int q, float mrp) {
-		this.mrqt = mrp*q;
-		this.Gst = (18*mrqt)/118;
-		this.Cgst = this.Sgst = Gst/2;
-		this.rate = mrqt - Gst;
-		this.FinalAmount = rate+Gst;
-	}
 
 	void Invoice() {
 		System.out.println("\tRate: "+rate);
@@ -345,5 +317,55 @@ class FivePercent extends GST {
 		System.out.println("\tSGST: "+Sgst);
 		System.out.println("\tGST: "+Gst);
 		System.out.println("\tMRP: "+FinalAmount);
+	}
+}
+
+class ZeroPercent extends GST {
+	ZeroPercent(int q, float mrp) {
+		this.mrqt = mrp*q;
+		this.Gst = (0*mrqt)/118;
+		this.Cgst = this.Sgst = Gst/2;
+		this.rate = mrqt - Gst;
+		this.FinalAmount = rate+Gst;
+	}
+}
+
+class FivePercent extends GST {
+	FivePercent(int q, float mrp) {
+		this.mrqt = mrp*q;
+		this.Gst = (5*mrqt)/118;
+		this.Cgst = this.Sgst = Gst/2;
+		this.rate = mrqt - Gst;
+		this.FinalAmount = rate+Gst;
+	}
+}
+
+class TwelvePercent extends GST {
+	TwelvePercent(int q, float mrp) {
+		this.mrqt = mrp*q;
+		this.Gst = (12*mrqt)/118;
+		this.Cgst = this.Sgst = Gst/2;
+		this.rate = mrqt - Gst;
+		this.FinalAmount = rate+Gst;
+	}
+}
+
+class EighteenPercent extends GST {
+	EighteenPercent(int q, float mrp) {
+		this.mrqt = mrp*q;
+		this.Gst = (18*mrqt)/118;
+		this.Cgst = this.Sgst = Gst/2;
+		this.rate = mrqt - Gst;
+		this.FinalAmount = rate+Gst;
+	}
+}
+
+class TwentyEightPercent extends GST {
+	TwentyEightPercent(int q, float mrp) {
+		this.mrqt = mrp*q;
+		this.Gst = (28*mrqt)/118;
+		this.Cgst = this.Sgst = Gst/2;
+		this.rate = mrqt - Gst;
+		this.FinalAmount = rate+Gst;
 	}
 }
